49e33390e745d26b2698ce5add8239b95cb25ba1
princeblack <mahamadicongo@hotmail.com>
2019-11-13 16:04:31 +0100

    update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const mongoose = require('mongoose');
 
 /** ROUTERS */
 const indexRouter = require('./routes/index');
@@ -13,30 +12,11 @@ const usersRouter = require('./routes/users');
 const recordsRouter = require('./routes/records');
 const ordersRouter = require('./routes/orders');
 
-/** OUR MIDDLEWARE */
-const { setCors } = require('./middleware/security');
-
 /** INIT THE SERVER */
 const app = express();
 
 /** LOGS */
 app.use(logger('dev'));
-
-/** CONNECT TO MONGO */
-mongoose.connect('mongodb://localhost:27017/live-coding-ds', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-
-mongoose.connection.on(
-  'error',
-  console.error.bind(console, 'connection error:')
-);
-
-mongoose.connection.on('open', () => {
-  console.log(`Connected to the database...`);
-});
 
 /** SET UP LOWDB */
 const adapter = new FileSync('data/db.json');
@@ -47,7 +27,6 @@ db.defaults({ records: [], users: [], orders: [] }).write();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(setCors);
 
 /** STATIC FILES */
 app.use(express.static(path.join(__dirname, 'public')));
@@ -57,20 +36,5 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/records', recordsRouter);
 app.use('/orders', ordersRouter);
-
-/** ERROR HANDLING */
-
-app.use(function(req, res, next) {
-  const err = new Error('Looks like something is broken...');
-  next(err);
-});
-
-app.use(function(err, req, res, next) {
-  res.status(400).send({
-    error: {
-      message: err.message
-    }
-  });
-});
 
 module.exports = app;
