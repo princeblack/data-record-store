f4758ae342bf6f0d1737a48a99a2820e15b0d060
princeblack <mahamadicongo@hotmail.com>
2019-12-05 09:54:13 +0100

    Login path

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,9 +5,8 @@ const createError = require('http-errors');
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find()
-    .select('-password -__v')
+    .select('-password -__v -tokens._id ')
     .sort('-lastName')
-    .limit(5)
     res.status(200).send(users);
   } catch (e) {
     next(e);
@@ -18,8 +17,13 @@ exports.addUser = async (req, res, next) => {
 
   try {
     const user = new User(req.body)
+    const token = user.generateAuthToken();
     await user.save();
-    res.status(200).send(user);
+    const data = user.getPublicFields();
+    res
+      .status(200)
+      .header('x-auth', token)
+      .send(user);
   } catch (e) {
     next(e)
   }
@@ -28,7 +32,7 @@ exports.addUser = async (req, res, next) => {
 exports.getUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id);
+    const user = await User.findById(id).select('-password -__v');
     if (!user) throw new createError.NotFound();
     res.status(200).send(user);
   } catch (e) {
@@ -40,7 +44,10 @@ exports.deleteUser =  async (req, res, next) => {
   try {
     const  user = await User.findByIdAndDelete(req.params.id);
     if (!user) throw new createError.NotFound();
-    res.status(200).send(user);
+    res
+      .status(200)
+      .send(user)
+      .select('-password');
   } catch (e) {
     next(e)
   }
@@ -53,8 +60,34 @@ exports.updateUser = async (req, res, next) => {
       runValidators: true
     });
     if (!user) throw new createError.NotFound();
+    const data = user.getPublicFields();
     res.status(200).send(user);
   } catch (e) {
     next(e)
   }
+};
+
+exports.loginUser = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await User.findOne({ email });
+    console.log(user);
+    const token = user.generateAuthToken();
+    const canLogin = await user.checkPassword(password);
+    if (!canLogin) throw new createError.NotFound();
+    const data = user.getPublicFields();
+
+    res
+      .status(200)
+      .header('x-auth', token)
+      .send(data);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.authenticateUser = async (req, res, next) => {
+  res.status(200).send(req.user);
 };
